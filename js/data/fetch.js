45ec9bf08ed1384114ab9fcc1313781d2f6d55b4
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-01-05 21:30:04 +0100

    typo

--- js/data/fetch.js
+++ js/data/fetch.js
@@ -1,6 +1,6 @@
 // const url = "https://api.noroff.dev/api/v1/square-eyes";
 const url =
-  "http://square-eyes.henrikbakke.no/wp-json/wc/store/products?per_page=20";
+  "https://square-eyes.henrikbakke.no/wp-json/wc/store/products?per_page=20";
 
 export async function makeApiCall() {
   const response = await fetch(url);
