02760841c350abd3d0bc6ff680421c35a6dac7cc
Henrik Bakke <119268924+Bakek-lang@users.noreply.github.com>
2024-01-07 21:59:44 +0100

    movies

--- js/data/fetch.js
+++ js/data/fetch.js
@@ -1,6 +1,6 @@
 // const url = "https://api.noroff.dev/api/v1/square-eyes";
 const url =
-  "https://square-eyes.henrikbakke.no/wp-json/wc/store/products?per_page=20";
+  "https://square-eyes.henrikbakke.no/wp-json/wc/store/products?per_page=25";
 
 export async function makeApiCall() {
   const response = await fetch(url);
